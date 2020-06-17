f9fdb6675fd2156b422bef1f844ea9fe2719b0e6
Steven Polido <steven.polido@vistage.com>
2020-06-17 11:59:30 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Chart from "./components/Chart";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chartData:{}
+    }
+  }
+  
+  componentDidMount() {
+    this.getChartData();
+  }
+
+  getChartData() {
+    this.setState({
+      chartData: {
+        labels: ['Los Angeles', 'San Diego', 'San Jose', 'San Francisco', 'Fresno', 'Sacramento', 'Long Beach'],
+        datasets: [
+            {
+                label: 'Population',
+                data: [
+                    4015936,
+                    1447100,
+                    1033673,
+                    896047,
+                    538195,
+                    521769,
+                    463218
+                ],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.6)',
+                    'rgba(54, 162, 235, 0.6)',
+                    'rgba(255, 206, 86, 0.6)',
+                    'rgba(75, 192, 192, 0.6)',
+                    'rgba(153, 102, 255, 0.6)',
+                    'rgba(255, 159, 64, 0.6)',
+                    'rgba(255, 99, 132, 0.6)',
+                ] 
+            }
+        ]
+    }
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <Chart chartData={ this.state.chartData } legendPosition="bottom" />
+      </div>
+    );
+  }
 }
 
 export default App;
